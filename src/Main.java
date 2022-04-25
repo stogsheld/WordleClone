78f33a7f80d90fae7d13ef865238abd2c12c5879
Oliver Jennings <94842011+stogsheld@users.noreply.github.com>
2022-04-25 23:17:03 +0100

    Added display functionality

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(WordsList.wordsOne.length);
-        System.out.println(WordsList2.wordsTwo.length);
-        System.out.println(WordsList3.wordsThree.length);
-        System.out.println(WordsList4.wordsFour.length);
-
         WordleClone wc = new WordleClone();
         wc.playGame();
     }
--- src/WordleClone.java
+++ src/WordleClone.java
@@ -6,55 +6,69 @@ import java.util.Scanner;
 public class WordleClone {
 
     public void playGame() {
-        ArrayList<String> attempts = new ArrayList();
+        ArrayList<String> attempts = new ArrayList<String>();
         int noOfAttempts = 0;
         displayTitle();
         Scanner scn = new Scanner(System.in);
         String correctWord = selectWord();
-        System.out.println(correctWord);
         while (noOfAttempts < 6) {
-            for (int i = 0; i < attempts.size(); i++) {
-                System.out.println(attempts.get(i));
+            for (String value : attempts) {
+                System.out.print("| ");
+                checkWord(value, correctWord);
+                System.out.print(" |");
             }
+            checkGuessesLeft(attempts.size());
+            System.out.println();
+            System.out.println();
             System.out.println("Enter your attempt:");
             String attempt = scn.nextLine();
-            System.out.println(attempt.length());
             if (attempt.length() == 5) {
-                if (attempt.equals(correctWord)) {
-                    System.out.println(DesignClass.GREEN_BOLD_BRIGHT + correctWord + " " + DesignClass.RESET);
-                    System.out.println("Correct! You win!");
-                    System.exit(0);
-                } else {
-                    for (int i = 0; i < attempt.length(); i++) {
-                        char currentCorrectLetter = correctWord.charAt(i);
-                        char currentAttemptLetter = attempt.charAt(i);
-                        String currentAttemptLetterString = Character.toString(currentAttemptLetter);
-                        int comparison = Character.compare(currentAttemptLetter, currentCorrectLetter);
-                        if (comparison == 0) {
-                            System.out.print(DesignClass.GREEN_BOLD_BRIGHT + currentAttemptLetter +
-                                    DesignClass.RESET);
-                        } else if (correctWord.contains(currentAttemptLetterString)) {
-                            System.out.print(DesignClass.YELLOW_BOLD_BRIGHT + currentAttemptLetterString +
-                                    DesignClass.RESET);
-                        } else {
-                            System.out.print(currentAttemptLetter);
-                        }
-                    }
-                    System.out.println();
-                }
+                checkWord(attempt, correctWord);
                 attempts.add(attempt);
                 noOfAttempts++;
+                System.out.println();
+                System.out.println();
             } else {
                 System.out.println("Please enter a 5 letter word.");
             }
         }
         System.out.println("Game Over.");
+        System.out.println("Correct word was " + DesignClass.GREEN_BOLD_BRIGHT + correctWord + DesignClass.RESET);
+    }
+
+    private void checkGuessesLeft(int size) {
+        for (int i = 0; i < (6-size); i++) {
+            System.out.print("| ⬜⬜⬜⬜⬜ |");
+        }
+    }
+
+    private void checkWord(String attempt, String correctWord) {
+        if (attempt.equals(correctWord)) {
+            System.out.println(DesignClass.GREEN_BOLD_BRIGHT + correctWord + " " + DesignClass.RESET);
+            System.out.println("Correct! You win!");
+            System.exit(0);
+        } else {
+            for (int i = 0; i < attempt.length(); i++) {
+                char currentCorrectLetter = correctWord.charAt(i);
+                char currentAttemptLetter = attempt.charAt(i);
+                String currentAttemptLetterString = Character.toString(currentAttemptLetter);
+                int comparison = Character.compare(currentAttemptLetter, currentCorrectLetter);
+                if (comparison == 0) {
+                    System.out.print(DesignClass.GREEN_BOLD_BRIGHT + currentAttemptLetter +
+                            DesignClass.RESET);
+                } else if (correctWord.contains(currentAttemptLetterString)) {
+                    System.out.print(DesignClass.YELLOW_BOLD_BRIGHT + currentAttemptLetterString +
+                            DesignClass.RESET);
+                } else {
+                    System.out.print(currentAttemptLetter);
+                }
+            }
+        }
     }
 
     private String selectWord() {
         String chosenWord = null;
         int wordBankChosen = randomiser(4) + 1;
-        System.out.println(wordBankChosen);
         int wordPlaceChosen = randomiser(3236);
         switch (wordBankChosen) {
             case 1:
@@ -82,6 +96,10 @@ public class WordleClone {
     }
 
     private void displayTitle() {
-        System.out.println("Wordle");
+        System.out.println("╦ ╦  ╔═╗  ╦═╗  ╔╦╗  ╦    ╔═╗\n" +
+                "║║║  ║ ║  ╠╦╝   ║║  ║    ║╣ \n" +
+                "╚╩╝  ╚═╝  ╩╚═  ═╩╝  ╩═╝  ╚═╝");
+        System.out.println();
     }
+
 }
